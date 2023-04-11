b386960b588aa6d2429592a52f8ff35ac974017d
greyhoundforty <ryantiffany@fastmail.com>
2023-04-11 11:44:20 -0500

    Adding updated logging

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ def setup_logger():
 
     options = {
         'index_meta': True,
-        'tags': 'testing-ce-private-endpoints',
+        'tags': 'test-cloud-endpoints',
         'url': 'https://logs.private.us-south.logging.cloud.ibm.com/logs/ingest',
-        'log_error_response': True
+        'log_error_response': True,
+        'env': 'development',
+        'app': 'test-cloud-endpoints'
     }
 
     logger = LogDNAHandler(key, options)
@@ -31,20 +33,22 @@ def setup_logger():
 def check_url(url):
     logger = setup_logger()
     response = requests.get(url)
-    logger.info(f"Status code for {url}: {response.status_code}")
+    logger.info(f"Status code for base URL {url}: {response.status_code}")
 
 def get_virtual_guests(apiEndpoint):
     logger = setup_logger()
     url = str(apiEndpoint + "/rest/v3/SoftLayer_Account/getVirtualGuests")
     headers = {"Authorization": f"Basic {username}:{api_key}"}
     response = requests.get(url, headers=headers)
-    logger.info(f"Status code for {url}: {response.status_code}")
+    logger.info(f"Status code for authenticated call to {url}: {response.status_code}")
 
 def main():
     logger = setup_logger()
+    logger.info("Testing public classic endpoints")
     check_url(publicEndpoint)
-    check_url(privateEndpoint)
     get_virtual_guests(publicEndpoint)
+    logger.info("Testing private classic endpoints")
+    check_url(privateEndpoint)
     get_virtual_guests(privateEndpoint)
 
 if __name__ == '__main__':
